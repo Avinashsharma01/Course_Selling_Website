d319926b5682e09f4bb49b2e62741536c043d0af
Avinash Sharma <Avinashsharma31384@gmail.com>
2025-07-02 12:01:36 +0530

    After setup the file structure

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is live")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is live on port number ${port}`);
+})
