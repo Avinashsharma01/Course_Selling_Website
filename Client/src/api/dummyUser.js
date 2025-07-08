7e492197612d7c4e2bb4b14ff86f27b3c89b0a2f
Gauri <160722158+gaurijakhmola@users.noreply.github.com>
2025-07-09 00:04:46 +0530

    user dashboard added

---
+++ Client/src/api/dummyUser.js
@@ -0,0 +1,12 @@
+// src/data/dummyUser.js
+
+const dummyUser = {
+  _id: "user_id",
+  name: "Gauri Jakhmola",
+  email: "gauri@learnhub.com",
+  profilePicture: "https://randomuser.me/api/portraits/women/75.jpg",
+  phoneNumber: "9876543210",
+  interests: ["Web Development", "UI/UX Design"],
+};
+
+export default dummyUser;
