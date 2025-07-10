e1d2f4d5b4e19f641cbaae0d5613946e77195eb5
ishikaaa18 <ishikasaha.trp@gmail.com>
2025-07-10 11:39:31 +0530

    privacy policy img updated

--- Client/src/pages/about/AboutUs.jsx
+++ Client/src/pages/about/AboutUs.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import teamImage from "../../assets/about-us.png";
+import React from "react";;
 import TrueFocus from "../../components/animations/TrueFocus";
 import Stack from "../../components/animations/Stack";
 import GridMotion from "../../components/animations/GridMotion";
